264e95bbdef68d002e0304607df356d11cc08940
Wahidul30 <e191021@ugrad.iiuc.ac.bd>
2022-02-18 11:15:52 +0600

    first commit

---
+++ another-banking.js
@@ -0,0 +1,28 @@
+function getInputValue(inputId) {
+  const inputField = document.getElementById(inputId);
+  const inputValue = parseFloat(inputField.value);
+  inputField.value = '';
+  return inputValue;
+}
+function updateTotal(totalId, amount) {
+  const fieldId = document.getElementById(totalId);
+  const previousTotal = parseFloat(fieldId.innerText);
+  const newTotal = previousTotal + amount;
+  fieldId.innerText = newTotal;
+}
+function updateBalanceTotal(amount, issAdding) {
+  const balanceTotalField = document.getElementById('balance-total');
+  const previousBalance = parseFloat(balanceTotalField.innerText);
+  const newBalance = previousBalance + amount;
+  balanceTotalField.innerText = newBalance;
+}
+document.getElementById('deposit-btn').addEventListener('click', function () {
+  const amount = getInputValue('deposit-input');
+  updateTotal('deposit-total', amount);
+  updateBalanceTotal(amount, true);
+})
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+  const amount = getInputValue('withdraw-input');
+  updateTotal('withdraw-total', amount);
+  updateBalanceTotal(amount, false);
+})
